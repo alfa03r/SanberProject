c0d24ab08f5adf454151409349c7153e5bd44750
alfa03r <muhammadalfajar218@gmail.com>
2024-05-18 15:29:05 +0700

    Task 8(PHP) : OOP (rev)

--- Task 8/oop/index.php
+++ Task 8/oop/index.php
@@ -12,16 +12,17 @@
 
     echo "Name : " . $kodok->name . "<br>";
     echo "Legs : " . $kodok->legs . "<br>";
-    echo "cold blooded : " . $kodok->cold_blooded . "<br><br>";
-    $kodok->jump();
+    echo "cold blooded : " . $kodok->cold_blooded . "<br>";
+    echo $kodok->jump();
+    echo "<br><br>";
 
     $sungokong = new Ape("kera sakti");
 
     echo "Name : " . $sungokong->name . "<br>";
     echo "Legs : " . $sungokong->legs . "<br>";
-    echo "cold blooded : " . $sungokong->cold_blooded . "<br><br>";
-    $sungokong->yell();
-    
+    echo "cold blooded : " . $sungokong->cold_blooded . "<br>";
+    echo$sungokong->yell();
+    echo "<br>";
     
     
 ?>
