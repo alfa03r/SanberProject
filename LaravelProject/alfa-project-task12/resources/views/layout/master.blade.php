fe8b402353e2ff064fe2f2b9c6f77821683d8ca2
alfa03r <muhammadalfajar218@gmail.com>
2024-05-28 15:39:52 +0700

    little update

--- LaravelProject/alfa-project-task12/resources/views/layout/master.blade.php
+++ LaravelProject/alfa-project-task12/resources/views/layout/master.blade.php
@@ -39,13 +39,7 @@
       <div class="container-fluid">
         <div class="row mb-2">
           <div class="col-sm-6">
-            <h1>Blank Page</h1>
-          </div>
-          <div class="col-sm-6">
-            <ol class="breadcrumb float-sm-right">
-              <li class="breadcrumb-item"><a href="#">Home</a></li>
-              <li class="breadcrumb-item active">Blank Page</li>
-            </ol>
+            <h1>@yield('title')</h1>
           </div>
         </div>
       </div><!-- /.container-fluid -->
@@ -57,7 +51,7 @@
       <!-- Default box -->
       <div class="card">
         <div class="card-header">
-          <h3 class="card-title">Title</h3>
+          <h3 class="card-title">@yield('title')</h3>
 
           <div class="card-tools">
             <button type="button" class="btn btn-tool" data-card-widget="collapse" title="Collapse">
@@ -69,7 +63,7 @@
           </div>
         </div>
         <div class="card-body">
-          Start creating your amazing application!
+          @yield('content')
         </div>
         <!-- /.card-body -->
         <div class="card-footer">
